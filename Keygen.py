6ac274ac1f3416601957fd3d10c5414d52a5c4f7
ruslan2020202 <ruslan020507@gmail.com>
2023-11-08 19:33:05 +0300

    init commit

---
+++ Keygen.py
@@ -0,0 +1,109 @@
+import setup
+import random
+import tqdm
+import time
+import prettytable
+from check_size_file import *
+
+
+class KeyGenerator:
+
+    def __init__(self):
+        self.alphabet = [chr(i) for i in range(97, 123)]
+        self.key_windows = []
+        self.commands()
+
+    def commands(self):
+        while True:
+            self.cmd = input(
+                '\nВыберите № команды для продолжения:\n\t1.Сгенерировать лицензионные ключи\n\t2.Показать сохранные ключи\n\t3.Выход в главное меню\n>>')
+            if self.cmd.strip() == '1':
+                self.selection()
+            elif self.cmd.strip() == '2':
+                self.saved_key()
+            elif self.cmd.strip() == '3':
+                setup.Generator()
+            else:
+                print(f'Ошибка\nКоманды {self.cmd} не существует\nПопробуйте еще раз')
+                continue
+
+    def selection(self):
+        while True:
+            self.choice_key = input(
+                '\nВыберите № ключа которого вы хотели бы сгенерировать(для выхода в меню введите exit):\n\t1.Windows OS\n>>')
+            match self.choice_key.strip().lower():
+                case '1':
+                    self.keygen_windows()
+                case 'exit':
+                    self.commands()
+                case _:
+                    print(f'Ошибка\nКоманды {self.choice_key} не существует\nПопробуйте еще раз')
+                    continue
+
+    def keygen_windows(self):
+        self.key_windows.clear()
+        for i in range(5):
+            self.key_assembly_completely = []
+            self.key_assembly_parts = []
+            for j in range(5):
+                for k in range(5):
+                    self.random_letter = random.choice(self.alphabet)
+                    self.random_num = random.randint(0, 9)
+                    x = random.randint(1, 2)
+                    match x:
+                        case 1:
+                            self.key_assembly_parts.append(self.random_letter.upper())
+                        case 2:
+                            self.key_assembly_parts.append(str(self.random_num))
+                self.key_assembly_completely.append(''.join(self.key_assembly_parts) + '-')
+                self.key_assembly_parts = []
+            self.key_windows.append((''.join(self.key_assembly_completely))[0:-1])
+        print('\nПодождите, лицензионные ключи генерируются...')
+        for i in tqdm.tqdm(range(1, 101)):
+            time.sleep(0.1)
+        print('Ключи активации успешно сгенерированы')
+        save_key = input('Хотите сохранить сгенерированные ключи(да/нет):')
+        while True:
+            if save_key.strip() == 'да':
+                print('Пароль успешно сохранен!')
+                with open('saved activion keys windows.txt', 'r') as file:
+                    strings = file.readlines()
+                n = len(strings)
+                f = open('saved activion keys windows.txt', 'a')
+                if empty_file('saved activion keys windows.txt'):
+                    f.write('Ключи активации Windows OS:\n')
+                    n += 1
+                for i in self.key_windows:
+                    f.write(f'{n}. {i}\n')
+                    n += 1
+                f.close()
+                self.commands()
+            elif save_key.strip() == 'нет':
+                print('Лицензионные ключи не были сохранены')
+                self.commands()
+            else:
+                print('Ошибка\nДля продолжения используйте только да/нет\nПопробуйте заново')
+                save_key = input('Хотите сохранить сгенерированные ключи(да/нет):')
+                continue
+        self.commands()
+
+    def saved_key(self):
+        print('Ваши лицензионные ключи:\n')
+        f = open(
+            'saved activion keys windows.txt',
+            'r')
+        file = 'saved activion keys windows.txt'
+        if empty_file(file):
+            print(
+                'Activioin keys not found!\nДля создания новых лицензионных ключей используйте команду "1.Сгенерировать лицензионные ключи"')
+        else:
+            table = prettytable.PrettyTable()
+            table.field_names = ['№', 'Activion keys']
+            for i in f:
+                if not 'Ключи' in i:
+                    x = i.split('.')
+                    x = list([j.strip() for j in x])
+                    table.add_row(x)
+            print(table)
+        f.close()
+        self.commands()
--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,4 @@
+import setup
 import random
 import time
 import os
@@ -5,40 +6,24 @@ import tqdm
 import prettytable
 
 
-class Generator:
+class PasswordGenerator:
+
     def __init__(self):
         self.alphabet = [chr(i) for i in range(97, 123)]
         self.saved_passwords = []
         self.dataCreationTime = []
-        self.key_assembly_windows = []
-        self.menu()
+        self.commands()
 
-    def menu(self):
-        while True:
-            self.menu_choice = input('Меню\n\t1.Генератор паролей\n\t2.Генератор ключей\n\t3.Выйти\n>>')
-            match self.menu_choice.strip().lower():
-                case '1':
-                    PasswordGenerator().commands_passwd()
-                case '2':
-                    KeyGenerator()
-                case '3':
-                    exit()
-                case _:
-                    print(f'Ошибка\nКоманды {self.menu_choice} не существует\nПопробуйте еще раз')
-                    continue
-
-
-class PasswordGenerator(Generator):
-    print('Генератор паролей')
-
-    def commands_passwd(self):  # функция команд
+    def commands(self):  # функция команд
         while True:
             self.cmd = input(
-                '\nВыберите № команды для продолжения:\n\t1.Сгенерировать рандомный пароль\n\t2.Показать сохраненные пароли\n>>')
+                '\nВыберите № команды для продолжения:\n\t1.Сгенерировать рандомный пароль\n\t2.Показать сохраненные пароли\n\t3.Выход в меню\n>>')
             if self.cmd.strip() == '1':
                 self.attribute()
             elif self.cmd.strip() == '2':
                 self.saved_passwd()
+            elif self.cmd.strip() == '3':
+                setup.Generator()
             else:
                 print(f'Ошибка\nКоманды {self.cmd} не существует\nПопробуйте еще раз')
                 continue
@@ -87,19 +72,17 @@ class PasswordGenerator(Generator):
                 self.data_create = time.asctime()
                 self.dataCreationTime.append(self.data_create)
                 self.saved_passwords.append(self.passwd)
-                f = open(
-                    '/my console projects/password generator + activion key/generated saved passwords.txt',
-                    'a')
+                f = open('generated saved passwords.txt', 'a')
                 data_passwd = self.saved_passwords.index(self.passwd)
                 with open('generated saved passwords.txt', 'r') as file:
                     old_password = file.readlines()
                 f.write(
                     f'{len(old_password) + 1}. {self.saved_passwords[data_passwd]} . (Дата создания : {self.dataCreationTime[data_passwd]})\n')
                 f.close()
-                self.commands_passwd()
+                self.commands()
             elif savepsswd.strip() == 'нет':
                 print('Пароль не был сохранен')
-                self.commands_passwd()
+                self.commands()
             else:
                 print('Ошибка\nДля продолжения используйте только да/нет\nПопробуйте заново')
                 savepsswd = input('Хотите сохранить пароль(да/нет):')
@@ -107,14 +90,14 @@ class PasswordGenerator(Generator):
     def saved_passwd(self):  # сохраненные пароли
         print('Ваши сохраненные пароли:\n')
         f = open(
-            '/my console projects/password generator + activion key/generated saved passwords.txt',
+            'generated saved passwords.txt',
             'r')
 
         def is_empty_file(file_name):  # проверка пуст ли файл.txt
             file_info = os.stat(file_name)
             return file_info.st_size == 0
 
-        file_name = '/my console projects/password generator + activion key/generated saved passwords.txt'
+        file_name = 'generated saved passwords.txt'
         if is_empty_file(file_name):
             print(
                 'Password not found!\nДля создания новых паролей используйте команду "1.Сгенерировать рандомный пароль"')
@@ -127,43 +110,4 @@ class PasswordGenerator(Generator):
                 table.add_row(x)
             print(table)
         f.close()
-        self.commands_passwd()
-
-
-class KeyGenerator(Generator):
-    print('Генератор ключей')
-
-    def commands_key(self):
-        while True:
-            self.cmnd = input(
-                '\nВыберите № команды для продолжения:\n\t1.Сгенерировать лицензионный ключ\n\t2.Показать сохранные ключи\n>>')
-            if self.cmnd.strip() == '1':
-                self.selection()
-            elif self.cmnd.strip() == '2':
-                self.saved_key()
-            else:
-                print(f'Ошибка\nКоманды {self.cmnd} не существует\nПопробуйте еще раз')
-                continue
-
-    def selection(self):
-        while True:
-            self.choice_key = input(
-                '\nВыберите № ключа которого вы хотели бы сгенерировать(для выхода в меню введите exit):\n\t1.Windows OS')
-            match self.choice_key.strip().lower():
-                case '1':
-                    self.keygen_windows()
-                case 'exit':
-                    pass
-                case _:
-                    print(f'Ошибка\nКоманды {self.choice_key} не существует\nПопробуйте еще раз')
-                    continue
-
-    def keygen_windows(self):
-        pass
-
-    def saved_key(self):
-        pass
-
-
-if __name__ == '__main__':
-    gen = Generator()
+        self.commands()
